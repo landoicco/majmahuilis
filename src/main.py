0d1a7ed4f84702099ffd9d0337221260b58d587a
Lando <landoicco@gmail.com>
2025-12-29 12:03:15 -0600

    Player class added and working

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
+
 from settings import *
+from player import *
 from map import *
 
 
@@ -9,19 +11,23 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
+        self.player.update()
         pygame.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption(f"{self.clock.get_fps():.1f}")
 
     def draw(self):
         self.screen.fill("black")
         self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pygame.event.get():
