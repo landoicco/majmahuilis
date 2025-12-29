41c91752cfdbd2554e3418f2bbc032bdf5dfd4fc
Lando <landoicco@gmail.com>
2025-12-29 10:48:19 -0600

    Map added and visible on game

--- src/main.py
+++ src/main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from majmahuilis!")
+import pygame
+import sys
+from settings import *
+from map import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        self.clock = pygame.time.Clock()
+        self.new_game()
+
+    def new_game(self):
+        self.map = Map(self)
+
+    def update(self):
+        pygame.display.flip()
+        self.clock.tick(FPS)
+        pygame.display.set_caption(f"{self.clock.get_fps():.1f}")
+
+    def draw(self):
+        self.screen.fill("black")
+        self.map.draw()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
+                pygame.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
 
 
 if __name__ == "__main__":
-    main()
+    game = Game().run()
